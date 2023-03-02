203ddeadddd7e92e29c495857a30973114191b52
MS20 YJChen5 <YJCHEN5@nuvoton.com>
2023-03-02 14:37:21 +0800

    1. Update SPI_Loopback sample code. 2. Update Windows driver of USBD_Printer_And_HID_Transfer sample code.
    Change-Id: Ibf2818e9ae0470958d18f1d5470fe89ec5057ab9

--- SampleCode/RegBased/SPI_Loopback/main.c
+++ SampleCode/RegBased/SPI_Loopback/main.c
@@ -1,22 +1,41 @@
-/******************************************************************************
+/**************************************************************************//**
  * @file     main.c
- * @version  V0.10
- * $Revision: 6 $
- * $Date: 15/09/02 10:03a $
- * @brief
- *           Implement SPI Master loop back transfer.
- *           This sample code needs to connect SPI0_MISO0 pin and SPI0_MOSI0 pin together.
- *           It will compare the received data with transmitted data.
- * @note
- * Copyright (C) 2014~2015 Nuvoton Technology Corp. All rights reserved.
-*****************************************************************************/
+ * @version  V3.00
+ * @brief    Demonstrate one SPI Master self-loopback transfer and two SPI 4-wire/3-wire loopback transfer.
+ *           SPI1 will be configured as Master mode and SPI0 will be configured as Slave mode.
+ *
+ * @copyright SPDX-License-Identifier: Apache-2.0
+ * @copyright Copyright (C) 2023 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
 #include <stdio.h>
 #include "M451Series.h"
 
-#define TEST_COUNT             64
+//*** <<< Use Configuration Wizard in Context Menu >>> ***
+// <e> Two SPI port loopback transfer
+#define TwoPortLoopback     0
+//  <o> Bi-direction Interface
+//  <0=> 4-wire <1=> 3-wire
+#define Slave3WireMode      0
+// </e>
+//*** <<< end of configuration section >>> ***
 
-uint32_t g_au32SourceData[TEST_COUNT];
-uint32_t g_au32DestinationData[TEST_COUNT];
+#define PLLCTL_SETTING      CLK_PLLCTL_72MHz_HXT
+#define PLL_CLOCK           72000000
+
+#define TEST_COUNT          64
+
+/* Global variable declaration */
+#if (!TwoPortLoopback)
+static uint32_t s_au32SourceData[TEST_COUNT];
+static uint32_t s_au32DestinationData[TEST_COUNT];
+#else
+static uint32_t s_au32MasterToSlaveTestPattern[TEST_COUNT];
+static uint32_t s_au32SlaveToMasterTestPattern[TEST_COUNT];
+static uint32_t s_au32MasterRxBuffer[TEST_COUNT];
+static uint32_t s_au32SlaveRxBuffer[TEST_COUNT];
+static volatile uint32_t s_u32MasterTxDataCount, s_u32MasterRxDataCount;
+static volatile uint32_t s_u32SlaveTxDataCount, s_u32SlaveRxDataCount;
+#endif
 
 /* Function prototype declaration */
 void SYS_Init(void);
@@ -28,12 +47,17 @@ void SPI_Init(void);
 /* ------------- */
 int main(void)
 {
-    uint32_t u32DataCount, u32TestCount, u32Err, u32TimeOutCnt;
+#if (!TwoPortLoopback)
+    uint32_t u32TestCount, u32Err, u32TimeOutCnt;
+#endif
+    uint32_t u32DataCount;
 
     /* Unlock protected registers */
     SYS_UnlockReg();
+
     /* Init System, IP clock and multi-function I/O. */
     SYS_Init();
+
     /* Lock protected registers */
     SYS_LockReg();
 
@@ -45,32 +69,25 @@ int main(void)
 
     printf("\n\n");
     printf("+------------------------------------------------------------------+\n");
-    printf("|                   M451 SPI Driver Sample Code                    |\n");
+    printf("|                      SPI Driver Sample Code                      |\n");
     printf("+------------------------------------------------------------------+\n");
     printf("\n");
-    printf("\nThis sample code demonstrates SPI0 self loop back data transfer.\n");
-    printf(" SPI0 configuration:\n");
+#if (!TwoPortLoopback)
+    printf("This sample code demonstrates SPI1 self loop back data transfer.\n");
+    printf(" SPI1 configuration:\n");
     printf("     Master mode; data width 32 bits.\n");
     printf(" I/O connection:\n");
-    printf("     PB.5 SPI0_MOSI0 <--> PB.3 SPI0_MISO0 \n");
-
-    printf("\nSPI0 Loopback test ");
-
-    /* set the source data and clear the destination buffer */
-    for(u32DataCount = 0; u32DataCount < TEST_COUNT; u32DataCount++)
-    {
-        g_au32SourceData[u32DataCount] = u32DataCount;
-        g_au32DestinationData[u32DataCount] = 0;
-    }
+    printf("     SPI1_MOSI(PA5) <--> SPI1_MISO(PA6)\n");
+    printf("\nSPI1 Loopback test ");
 
     u32Err = 0;
     for(u32TestCount = 0; u32TestCount < 0x1000; u32TestCount++)
     {
-        /* set the source data and clear the destination buffer */
+        /* Set the source data and clear the destination buffer */
         for(u32DataCount = 0; u32DataCount < TEST_COUNT; u32DataCount++)
         {
-            g_au32SourceData[u32DataCount]++;
-            g_au32DestinationData[u32DataCount] = 0;
+            s_au32SourceData[u32DataCount] = u32DataCount;
+            s_au32DestinationData[u32DataCount] = 0;
         }
 
         u32DataCount = 0;
@@ -83,11 +100,11 @@ int main(void)
         while(1)
         {
             /* Write to TX register */
-            SPI_WRITE_TX(SPI0, g_au32SourceData[u32DataCount]);
+            SPI_WRITE_TX(SPI1, s_au32SourceData[u32DataCount]);
 
-            /* Check SPI0 busy status */
+            /* Check SPI1 busy status */
             u32TimeOutCnt = SystemCoreClock; /* 1 second time-out */
-            while(SPI_IS_BUSY(SPI0))
+            while(SPI_IS_BUSY(SPI1))
             {
                 if(--u32TimeOutCnt == 0)
                 {
@@ -101,7 +118,7 @@ int main(void)
                 break;
 
             /* Read received data */
-            g_au32DestinationData[u32DataCount] = SPI_READ_RX(SPI0);
+            s_au32DestinationData[u32DataCount] = SPI_READ_RX(SPI1);
             u32DataCount++;
             if(u32DataCount == TEST_COUNT)
                 break;
@@ -113,7 +130,7 @@ int main(void)
         /*  Check the received data */
         for(u32DataCount = 0; u32DataCount < TEST_COUNT; u32DataCount++)
         {
-            if(g_au32DestinationData[u32DataCount] != g_au32SourceData[u32DataCount])
+            if(s_au32DestinationData[u32DataCount] != s_au32SourceData[u32DataCount])
                 u32Err = 1;
         }
 
@@ -126,15 +143,82 @@ int main(void)
     else
         printf(" [PASS]\n\n");
 
+    /* Disable SPI1 peripheral clock */
+    CLK->APBCLK0 &= (~CLK_APBCLK0_SPI1CKEN_Msk);
+#else
+#if (Slave3WireMode)
+    printf("This sample code demonstrates Slave 3-wire mode loop back transfer.\n\n");
+#else
+    printf("This sample code demonstrates SPI0/SPI1 loop back transfer.\n\n");
+#endif
+    printf("Configure SPI0 as a slave and SPI1 as a master.\n");
+    printf("Bit length of a transaction: 32\n");
+    printf("Please connect below I/O connections for SPI0 and SPI1:\n");
+#if (!Slave3WireMode)
+    printf("    SPI0_SS   (PB4)   <->   SPI1_SS  (PA4)\n");
+#endif
+    printf("    SPI0_CLK  (PB2)   <->   SPI1_CLK (PA7)\n");
+    printf("    SPI0_MISO0(PB3)   <->   SPI1_MISO(PA6)\n");
+    printf("    SPI0_MOSI0(PB5)   <->   SPI1_MOSI(PA5)\n\n");
+    printf("After the transfer is done, the received data will be printed out.\n");
+
+    for(u32DataCount = 0; u32DataCount < TEST_COUNT; u32DataCount++)
+    {
+        /* Write the initial value to source buffer */
+        s_au32MasterToSlaveTestPattern[u32DataCount] = 0x00550000 + u32DataCount;
+        s_au32SlaveToMasterTestPattern[u32DataCount] = 0x00AA0000 + u32DataCount;
+        /* Clear destination buffer */
+        s_au32MasterRxBuffer[u32DataCount] = 0;
+        s_au32SlaveRxBuffer[u32DataCount] = 0;
+    }
+
+    s_u32MasterTxDataCount = 0;
+    s_u32MasterRxDataCount = 0;
+    s_u32SlaveTxDataCount = 0;
+    s_u32SlaveRxDataCount = 0;
+    printf("Press any key to start transmission ...\n");
+    getchar();
+    printf("\n");
+
+    /* Access TX and RX FIFO */
+    while((s_u32MasterRxDataCount < TEST_COUNT) || (s_u32SlaveRxDataCount < TEST_COUNT))
+    {
+        /* Check TX FULL flag and TX data count */
+        if((SPI_GET_TX_FIFO_FULL_FLAG(SPI1) == 0) && (s_u32MasterTxDataCount < TEST_COUNT))
+            SPI_WRITE_TX(SPI1, s_au32MasterToSlaveTestPattern[s_u32MasterTxDataCount++]); /* Write to TX FIFO */
+        /* Check TX FULL flag and TX data count */
+        if((SPI_GET_TX_FIFO_FULL_FLAG(SPI0) == 0) && (s_u32SlaveTxDataCount < TEST_COUNT))
+            SPI_WRITE_TX(SPI0, s_au32SlaveToMasterTestPattern[s_u32SlaveTxDataCount++]); /* Write to TX FIFO */
+        /* Check RX EMPTY flag */
+        if(SPI_GET_RX_FIFO_EMPTY_FLAG(SPI1) == 0)
+            s_au32MasterRxBuffer[s_u32MasterRxDataCount++] = SPI_READ_RX(SPI1); /* Read RX FIFO */
+        /* Check RX EMPTY flag */
+        if(SPI_GET_RX_FIFO_EMPTY_FLAG(SPI0) == 0)
+            s_au32SlaveRxBuffer[s_u32SlaveRxDataCount++] = SPI_READ_RX(SPI0); /* Read RX FIFO */
+    }
+
+    /* Print the received data */
+    printf("\tSPI0 Received data:\tSPI1 Received data:\n");
+    for(u32DataCount = 0; u32DataCount < TEST_COUNT; u32DataCount++)
+    {
+        printf("%d:\t0x%X\t\t0x%X\n", u32DataCount, s_au32SlaveRxBuffer[u32DataCount], s_au32MasterRxBuffer[u32DataCount]);
+    }
+
+    printf("The data transfer was done.\n");
+
+    printf("\n\nExit SPI driver sample code.\n");
+
     /* Disable SPI0 peripheral clock */
     CLK->APBCLK0 &= (~CLK_APBCLK0_SPI0CKEN_Msk);
+    /* Disable SPI1 peripheral clock */
+    CLK->APBCLK0 &= (~CLK_APBCLK0_SPI1CKEN_Msk);
+#endif
 
     while(1);
 }
 
 void SYS_Init(void)
 {
-
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init System Clock                                                                                       */
     /*---------------------------------------------------------------------------------------------------------*/
@@ -148,13 +232,29 @@ void SYS_Init(void)
     /* Select HXT as the clock source of HCLK */
     CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLKSEL_Msk)) | CLK_CLKSEL0_HCLKSEL_HXT;
 
+    /* Set PLL to Power-down mode */
+    CLK->PLLCTL |= CLK_PLLCTL_PD_Msk;
+
+    /* Configure PLL */
+    CLK->PLLCTL = PLLCTL_SETTING;
+
+    /* Waiting for clock ready */
+    while(!(CLK->STATUS & CLK_STATUS_PLLSTB_Msk));
+
+    /* Select PLL as the system clock source */
+    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLKSEL_Msk)) | CLK_CLKSEL0_HCLKSEL_PLL;
+
     /* Select HXT as the clock source of UART */
     CLK->CLKSEL1 = (CLK->CLKSEL1 & (~CLK_CLKSEL1_UARTSEL_Msk)) | CLK_CLKSEL1_UARTSEL_HXT;
+
     /* Select PCLK0 as the clock source of SPI0 */
     CLK->CLKSEL2 = (CLK->CLKSEL2 & (~CLK_CLKSEL2_SPI0SEL_Msk)) | CLK_CLKSEL2_SPI0SEL_PCLK0;
 
-    /* Enable UART0 and SPI0 clock */
-    CLK->APBCLK0 = CLK_APBCLK0_UART0CKEN_Msk | CLK_APBCLK0_SPI0CKEN_Msk;
+    /* Select PCLK1 as the clock source of SPI1 */
+    CLK->CLKSEL2 = (CLK->CLKSEL2 & (~CLK_CLKSEL2_SPI1SEL_Msk)) | CLK_CLKSEL2_SPI1SEL_PCLK1;
+
+    /* Enable UART0, SPI0 and SPI1 clock */
+    CLK->APBCLK0 = CLK_APBCLK0_UART0CKEN_Msk | CLK_APBCLK0_SPI0CKEN_Msk | CLK_APBCLK0_SPI1CKEN_Msk;
 
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init I/O Multi-function                                                                                 */
@@ -164,14 +264,25 @@ void SYS_Init(void)
     SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD0MFP_Msk | SYS_GPD_MFPL_PD1MFP_Msk);
     SYS->GPD_MFPL |= (SYS_GPD_MFPL_PD0MFP_UART0_RXD | SYS_GPD_MFPL_PD1MFP_UART0_TXD);
 
+#if (TwoPortLoopback)
     /* Set SPI0 multi-function pins */
     SYS->GPB_MFPL &= ~(SYS_GPB_MFPL_PB2MFP_Msk | SYS_GPB_MFPL_PB3MFP_Msk | SYS_GPB_MFPL_PB4MFP_Msk | SYS_GPB_MFPL_PB5MFP_Msk);
-    SYS->GPB_MFPL |= (SYS_GPB_MFPL_PB2MFP_SPI0_CLK | SYS_GPB_MFPL_PB3MFP_SPI0_MISO0 | SYS_GPB_MFPL_PB4MFP_SPI0_SS | SYS_GPB_MFPL_PB5MFP_SPI0_MOSI0);
+    SYS->GPB_MFPL |= (SYS_GPB_MFPL_PB2MFP_SPI0_CLK | SYS_GPB_MFPL_PB3MFP_SPI0_MISO0 | SYS_GPB_MFPL_PB5MFP_SPI0_MOSI0);
+#if (!Slave3WireMode)
+    SYS->GPB_MFPL |= SYS_GPB_MFPL_PB4MFP_SPI0_SS;
+#endif
+#endif
+
+    /* Set SPI1 multi-function pins */
+    SYS->GPA_MFPL &= ~(SYS_GPA_MFPL_PA4MFP_Msk | SYS_GPA_MFPL_PA5MFP_Msk | SYS_GPA_MFPL_PA6MFP_Msk | SYS_GPA_MFPL_PA7MFP_Msk);
+    SYS->GPA_MFPL |= (SYS_GPA_MFPL_PA5MFP_SPI1_MOSI | SYS_GPA_MFPL_PA6MFP_SPI1_MISO | SYS_GPA_MFPL_PA7MFP_SPI1_CLK);
+#if (!Slave3WireMode)
+    SYS->GPA_MFPL |= SYS_GPA_MFPL_PA4MFP_SPI1_SS;
+#endif
 
     /* Update System Core Clock */
     /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and CyclesPerUs automatically. */
     SystemCoreClockUpdate();
-
 }
 
 void UART0_Init(void)
@@ -187,12 +298,21 @@ void SPI_Init(void)
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init SPI                                                                                                */
     /*---------------------------------------------------------------------------------------------------------*/
-    /* Set IP clock divider. SPI clock rate = f_PCLK0 / (5+1) */
-    SPI0->CLKDIV = SPI0->CLKDIV & (~SPI_CLKDIV_DIVIDER_Msk) | 5;
+    /* Set IP clock divider. SPI clock rate = f_PCLK1 / (35+1) */
+    SPI1->CLKDIV = (SPI1->CLKDIV & (~SPI_CLKDIV_DIVIDER_Msk)) | 35;
     /* Configure as a master, clock idle low, 32-bit transaction, drive output on falling clock edge and latch input on rising edge. */
-    SPI0->CTL = SPI_MASTER | SPI_CTL_TXNEG_Msk | SPI_CTL_SPIEN_Msk;
+    SPI1->CTL = SPI_MASTER | SPI_CTL_TXNEG_Msk | SPI_CTL_SPIEN_Msk;
+    /* Enable the automatic hardware slave select function. Select the SS pin and configure as low-active. */
+    SPI1->SSCTL = SPI_SSCTL_AUTOSS_Msk | SPI_SSCTL_SS_Msk;
 
+#if (TwoPortLoopback)
+    /* Configure SPI0 */
+#if (Slave3WireMode)
+    /* Enable slave 3-wire mode before enabling SPI controller */
+    SPI0->SSCTL = SPI_SSCTL_SLV3WIRE_Msk;
+#endif
+    /* Configure SPI0 as a slave, clock idle low, 32-bit transaction, drive output on falling clock edge and latch input on rising edge. */
+    /* Configure SPI0 as a low level active device. SPI peripheral clock rate = f_PCLK0 */
+    SPI0->CTL = SPI_SLAVE | SPI_MODE_0 | SPI_CTL_SPIEN_Msk;
+#endif
 }
-
-/*** (C) COPYRIGHT 2014~2015 Nuvoton Technology Corp. ***/
-
