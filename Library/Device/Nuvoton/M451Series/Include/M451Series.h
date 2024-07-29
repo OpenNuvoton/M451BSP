e67abdabbd5ab74cbdae998c07da18e02d2ac4d0
“YTLu” <YTLu@nuvoton.com>
2024-07-29 09:55:46 +0800

    Modify Readme, Revision history and chm files.
    Change-Id: I46a88142200b315e9ca02846bedb04cedd170d5c

--- Library/Device/Nuvoton/M451Series/Include/M451Series.h
+++ Library/Device/Nuvoton/M451Series/Include/M451Series.h
@@ -27,7 +27,7 @@
   *
   * <b>Copyright Notice</b>
   *
-  * Copyright (C) 2014~2015 Nuvoton Technology Corp. All rights reserved.
+  * Copyright (C) 2014~2024 Nuvoton Technology Corp. All rights reserved.
   */
 
 #ifndef __M451SERIES_H__
